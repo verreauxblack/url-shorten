c3087c7ee3a3241fa2e7125c4a9c1e09483a48ec
RAJ <verreauxblack@outlook.com>
2021-07-08 16:40:11 +0530

    navbar & section1 added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Navbar from "./components/navbar";
+import Section1 from "./components/section1";
+
+import './sass/main.sass'
+
 const App = () => {
     return (
-        <div>
-            URL shorten
-        </div>
+        <main>
+            <Navbar />
+            <Section1 />
+        </main>
     )
 }
 
