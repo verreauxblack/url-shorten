5bf40980522741f4286d5a703a8ee7fc4ba8d956
RAJ <verreauxblack@outlook.com>
2021-07-09 09:59:48 +0530

    Boost section completed, now responsive

---
+++ src/components/Boost.jsx
@@ -0,0 +1,12 @@
+import Design from '../images/bg-boost-desktop.svg'
+const Boost = () => {
+    return (
+        <section className="boost">
+            <img src={Design} alt="" />
+            <h1>Boost your links today</h1>
+            <button>Get started</button>
+        </section>
+    )
+}
+
+export default Boost
