9eaaa7a0b2c5085f987c031c1cf4f191af856780
RAJ <verreauxblack@outlook.com>
2021-07-09 16:15:02 +0530

    ipad size completed, 50% responsive

--- src/components/Boost.jsx
+++ src/components/Boost.jsx
@@ -1,8 +1,10 @@
 import Design from '../images/bg-boost-desktop.svg'
+import DesignMobile from '../images/bg-boost-mobile.svg'
 const Boost = () => {
     return (
         <section className="boost">
-            <img src={Design} alt="" />
+            <img id="desk" src={Design} alt="" />
+            <img id="mobile" src={DesignMobile} alt="" />
             <h1>Boost your links today</h1>
             <button>Get started</button>
         </section>
